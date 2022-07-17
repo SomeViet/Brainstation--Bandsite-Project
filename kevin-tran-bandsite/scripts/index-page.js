39a9e67332c2edec42031cc2dd7fa8c567117f11
Kevin Tran <kevtran@hotmail.com>
2022-07-16 21:36:17 -0700

    Comment Section Done

--- kevin-tran-bandsite/scripts/index-page.js
+++ kevin-tran-bandsite/scripts/index-page.js
@@ -1,27 +1,88 @@
-let userhistory = ["Miles Acosta", "Emilie Beach", "Connor Walton"];
-let commenthistory = [
-    "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-    "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-    "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+let commentHistory = [
+    {
+        userName: "Miles Acosta",
+        comment:
+            "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+        dateHistory: "12/20/2020",
+        avatarSource: "",
+    },
+    {
+        userName: "Kevin Tran",
+        comment:
+            "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+        dateHistory: "01/01/19",
+        avatarSource: "",
+    },
 ];
-let datehistory = ["12/20/2020", "01/09/2021", "02/17/2021"];
-let avatarhistory = [];
 
-const form = document.getElementById("comments");
+let historyUpdate = () => {
+    for (let i = commentHistory.length - 1; i >= 0; i--) {
+        // Set-up Tags for Loop
+        let paste = document.querySelector(".comments__history");
+        let userSetup = document.createElement("h6");
+        let dateSetup = document.createElement("h6");
+        let commentSetup = document.createElement("p");
+        let avatarSetup = document.createElement("img");
+        let list = document.createElement("li");
+        console.log(commentHistory.length);
+
+        // New User
+        userSetup.innerText = commentHistory[i].userName;
+        userSetup.classList.add("comments__history-name");
+
+        // New Comment
+        commentSetup.innerText = commentHistory[i].comment;
+        commentSetup.classList.add("comments__history-comment");
+
+        // Date Setup
+        dateSetup.innerText = commentHistory[i].dateHistory;
+        dateSetup.classList.add("comments__history-timestamp");
+
+        // Avatar Setup
+        avatarSetup.innerText = commentHistory[i].avatarSource;
+        avatarSetup.classList.add("comments__avatar");
+        // not applicable for sprint 2
+        avatarSetup.setAttribute("src", "#");
+
+        // Structure Setup
+        let divHeader = document.createElement("div");
+        divHeader.classList.add("comments__history-header");
+        let divContainer = document.createElement("div");
+        divContainer.classList.add("comments__history-right-container");
+
+        // Piecing it together
+        list.appendChild(avatarSetup);
+        divHeader.appendChild(userSetup);
+        divHeader.appendChild(dateSetup);
+        divContainer.appendChild(divHeader);
+        divContainer.appendChild(commentSetup);
+        list.appendChild(divContainer);
+        list.classList.add("comments__history-list-item");
+        paste.appendChild(list);
+    }
+};
+
+let clear = () => {
+    let reset = document.querySelector(".comments__history");
+    reset.innerHTML = "";
+};
+
+historyUpdate();
+
+const form = document.querySelector(".comments__form");
 form.addEventListener("submit", function (convo) {
     convo.preventDefault();
-    let date1 = new Date();
-    userhistory.push(convo.target.user_name.value);
-    commenthistory.push(convo.target.user_comment.value);
-    datehistory.push(date1.toLocaleDateString());
-    //To delete console logs when done.
-    console.log(userhistory);
-    console.log(commenthistory);
-    console.log(datehistory);
+    let dateNow = new Date();
+    let commentClear = document.querySelector(".comments__comment-input");
+    commentHistory.push({
+        userName: convo.target.user_name.value,
+        comment: convo.target.user_comment.value,
+        dateHistory: dateNow.toLocaleDateString(),
+        // not applicable for sprint 2
+        avatar: "",
+    });
+    console.log(commentHistory);
+    clear();
+    historyUpdate();
+    commentClear.value = "";
 });
-
-// let form2 = document.getElementById("commentsgohere");
-// let form2comments = document.createElement("div");
-// form2comments.classList.add("comment-red");
-// form2comments.innerText = "YAY";
-// form2.appendChild(form2comments);
